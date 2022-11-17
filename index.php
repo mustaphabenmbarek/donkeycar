140421179482dadb573187ba5a1613851387d435
mustaphabenmbarek <mbenmbarek91@gmail.com>
2022-11-17 12:07:19 +0100

    version initial index

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Réservation voiture</title>
+    </head>
+    <body>
+
+
+    </body>
+</html>
