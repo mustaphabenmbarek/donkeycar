844e42278682ba5407f3e6843d76d2206cfad964
Siondubois <sion.p.ly@gmail.com>
2022-11-24 11:12:53 +0100

    ajout de cars.php

---
+++ cars.php
@@ -0,0 +1,45 @@
+<?php require 'header.php';?>
+
+<body>
+    <div class = "container-fluid">
+        <div class="row">
+          <div class="col-sm-4">
+            <img src="" alt="1">
+          </div>
+          <div class="col-sm-4">
+            <ul>
+              <li><img src="" alt="2"></li>
+              <li><img src="" alt="3"></li>
+              <li><img src="" alt="4"></li>
+            </ul>
+          </div>
+          <div class="col-sm-4">
+            <form action="" method="post">
+              <div class="resa">
+                <label for="name">reservez votre véhicule</label>
+                <input type="text" name="name" id="name" required>
+              </div>
+              <div class="form-example">
+                <label for="email">Enter your email: </label>
+                <input type="email" name="email" id="email" required>
+              </div>
+              <div class="form-example">
+                <input type="submit" value="Subscribe!">
+              </div>
+            </form>
+          </div>
+        </div>
+    </div>
+
+    <div class = "container">
+      <div class = "col -sm-6">
+        description voiture
+      </div>
+      <div class = "col -sm-6">
+        avis client
+      </div>
+      
+    </div>
+    
+</body>
+</html>
